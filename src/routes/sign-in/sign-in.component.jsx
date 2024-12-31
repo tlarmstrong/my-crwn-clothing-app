659ede5eacd0d201bdd3f21a487a68301a5b6c46
tracey <tracelyn.armstrong@gmail.com>
2024-12-31 15:34:27 -0700

    created components for sign-up, form inputs, and buttons

---
+++ src/routes/sign-in/sign-in.component.jsx
@@ -0,0 +1,27 @@
+import { getRedirectResult } from 'firebase/auth';
+
+import { 
+  auth,
+  signInWithGooglePopup, 
+  // signInWithGoogleRedirect, 
+  createUserDocumentFromAuth 
+} from '../../utils/firebase/firebase.utils';
+
+import SignUpForm from '../../components/sign-up-form/sign-up-form.component';
+
+const SignIn = () => {
+  const logGoogleUser = async () => {
+    const { user } = await signInWithGooglePopup();
+    const userDocRef = await createUserDocumentFromAuth(user);
+  }
+
+  return (
+      <div>
+        <h1>Sign In Page</h1>
+        <button onClick={ logGoogleUser } >Sign in with Google Popup</button>
+        <SignUpForm />
+      </div>
+  );
+}
+
+export default SignIn;
