8effac63dd166470290e249c10259dcd6dafa0cf
tracey <tracelyn.armstrong@gmail.com>
2024-12-31 17:03:02 -0700

    refactor authentication, add ability to signin with username and password

---
+++ src/components/sign-in-form/sign-in-form.component.jsx
@@ -0,0 +1,86 @@
+import { useState } from 'react';
+
+import FormInput from '../form-input/form-input.component';
+import Button from '../button/button.component';
+
+import { 
+  signInWithGooglePopup,
+  createUserDocumentFromAuth,
+  signInUserWithEmailAndPassword  
+} from '../../utils/firebase/firebase.utils';
+
+import './sign-in-form.styles.scss';
+
+const defaultFormFields = {
+  email: '',
+  password: ''
+}
+
+const SignInForm = () => {
+  const signInWithGoogle = async (event) => {
+    event.preventDefault();
+    const { user } = await signInWithGooglePopup();
+    await createUserDocumentFromAuth(user);
+  }
+
+  const [formFields, setFormFields] = useState(defaultFormFields);
+  const { email, password } = formFields;
+
+  console.log(formFields);
+
+  const resetFormFields = () => {
+    setFormFields(defaultFormFields);
+  }
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+
+    try {
+      const response = await signInUserWithEmailAndPassword(email, password);
+      console.log(response);
+      resetFormFields();
+    } catch(error) {
+        console.log('Error fetching user with username and password', error.message);
+    }
+  }
+
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+    setFormFields({ ...formFields, [name]: value });
+  }
+
+  return (
+    <div className='sign-in-container'>
+      <h2>Already have an account?</h2>
+      <span>Sign in with your email and password</span>
+      <form onSubmit={ handleSubmit } >
+        <FormInput 
+          label='Email'
+          inputOptions = {{
+            type: 'email',
+            required: true,
+            onChange: handleChange,
+            name: 'email',
+            value: email 
+          }} 
+        />
+        <FormInput
+          label='Password'
+          inputOptions = {{
+            type: 'password',
+            required: true,
+            onChange: handleChange,
+            name: 'password',
+            value: password 
+          }} 
+        />
+        <div className='buttons-container'>
+          <Button type='submit'>Sign In</Button>
+          <Button onClick={ signInWithGoogle } buttonType='google'>Use Google</Button>
+        </div>
+      </form>
+    </div>
+  );
+}
+
+export default SignInForm;
--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -7,7 +7,8 @@ import {
   getAuth,
   signInWithPopup,
   signInWithRedirect,
-  createUserWithEmailAndPassword
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
 } from "firebase/auth";
 
 import {
@@ -74,4 +75,10 @@ export const createAuthUserWithEmailAndPassword = async (email, password) => {
   if(!email || !password) return;
 
   return await createUserWithEmailAndPassword(auth, email, password);
+}
+
+export const signInUserWithEmailAndPassword = async (email, password) => {
+  if(!email || !password) return;
+  
+  return await signInWithEmailAndPassword(auth, email, password);
 }
