74cef60bee2ea31199ed07fb6d7164aa19132866
tracey <tracelyn.armstrong@gmail.com>
2025-01-03 08:32:53 -0700

    get categories and products from database and display

--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -20,6 +20,8 @@ import {
   setDoc,
   collection,
   writeBatch,
+  query,
+  getDocs,
 } from 'firebase/firestore';
 
 // Your web app's Firebase configuration
@@ -59,6 +61,21 @@ export const addCollectionAndDocuments = async (collectionKey, objectsToAdd) => 
   })
   await batch.commit();
   console.log('done');
+}
+
+export const getCategoriesAndDocuments = async () => {
+  const collectionRef = collection(db, 'categories');
+  const q = query(collectionRef);
+
+  const querySnapshot = await getDocs(q);
+  const categoryMap = querySnapshot.docs.reduce((acc, docSnapshot) => {
+    // acc = accumulator
+    const { title, items } = docSnapshot.data();
+    acc[title.toLowerCase()] = items;
+    return acc;
+  }, {});
+  
+  return categoryMap;
 }
 
 export const createUserDocumentFromAuth = async (
