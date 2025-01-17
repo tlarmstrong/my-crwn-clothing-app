1974534af2ac1cff3c4e7f06e9d043f8347c9696
tracey <tracelyn.armstrong@gmail.com>
2025-01-17 16:04:12 -0700

    add reselect and use to memoize categories (cache)

--- src/store/categories/category.selector.js
+++ src/store/categories/category.selector.js
@@ -1,12 +1,22 @@
 // selectors are where the data is transformed into the final shape we want
 // this is where the logic should live
 
-export const selectCategoriesMap = (state) => {
-  console.log('selecting categories and transforming into map');
-  return state.categories.categories.reduce((acc, category) => {
-    // acc = accumulator
-    const { title, items } = category;
-    acc[title.toLowerCase()] = items;
-    return acc;
-  }, {});
-};
+import { createSelector } from 'reselect';
+
+const selectCategoriesReducer = (state) => state.categories;
+
+export const selectCategories = createSelector(
+  [selectCategoriesReducer], // input
+  (categoriesSlice) => categoriesSlice.categories // output
+);
+
+export const selectCategoriesMap = createSelector(
+  [selectCategories],
+  (categories) => 
+    categories.reduce((acc, category) => {
+      const { title, items } = category;
+      // acc = accumulator
+      acc[title.toLowerCase()] = items;
+      return acc;
+    }, {})
+);
