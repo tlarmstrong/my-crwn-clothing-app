ae61ee1bb9340c730ced76406b71939a9c9eaf82
tracey <tracelyn.armstrong@gmail.com>
2025-01-19 13:39:31 -0700

    cleanup debugging code

--- src/routes/category/category.component.jsx
+++ src/routes/category/category.component.jsx
@@ -10,13 +10,11 @@ import { CategoryTitle, CategoryContainer } from './category.styles';
 
 const Category = () => {
   const { category } = useParams();
-  console.log('render/rerendering category component');
   const categoriesMap = useSelector(selectCategoriesMap);
   const [products, setProducts] = useState(categoriesMap[category]);
 
   // only want to update if the category or category map changes
   useEffect(() => {
-    console.log('effect fired calling setProducts');
     setProducts(categoriesMap[category]);
   }, [category, categoriesMap]);
 
